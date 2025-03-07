772ccdcd3311ecca7710c01ba5c97e60587e4d8f
KKMIA793\kkmia <kkmia793@gmail.com>
2025-03-07 18:46:12 +0900

    feat : タイトルシーンからモード選択シーンへの遷移

---
+++ Assets/Scripts/Title/TitleModel.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TitleModel
+{
+    public string StartMessage { get; private set; }
+
+    public TitleModel()
+    {
+        StartMessage = "A";
+    }
+    
+}
---
+++ Assets/Scripts/Title/TitleView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TitleView : MonoBehaviour
+{
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private TextMeshProUGUI startMessageText;
+
+    public void SetStartMessage(string message)
+    {
+        if (startMessageText != null)
+        {
+            startMessageText.text = message;
+        }
+    }
+
+    public void ShowTitleScreen(bool show)
+    {
+        if (titleScreen != null)
+        {
+            titleScreen.SetActive(show);
+        }
+    }
+    
+}
